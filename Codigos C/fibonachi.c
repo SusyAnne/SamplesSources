b084c2c06968af18fe04b7f45b376f789ded87c0
suysgp <susysgp@gmail.com>
2014-03-31 20:32:23 -0300

    sequencia fibonachi

---
+++ Codigos C/fibonachi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main () {
+
+   int a;
+   int i = 0;
+
+   printf("Digite um numero");
+   scanf("%d",&a);
+
+   printf("Sequencia Fibonacci\n");
+
+   while(i>0)
+    {
+        printf("%d \n", i);
+        i ++;
+    }
+}
